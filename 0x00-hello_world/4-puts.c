59dac380e41c9fad5e9261c860743f96748b63ed
trainingmb <availfolly@yahoo.com>
2022-12-08 08:07:34 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,8 +8,6 @@
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
-
 	puts(str);
-
 	return (0);
 }
