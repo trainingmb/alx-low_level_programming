88743f87f9a0f3230894a641066363dc4e0f812e
trainingmb <availfolly@yahoo.com>
2022-12-09 12:05:13 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Print single digit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int n = '0';
+
+		while (n <= '9')
+		{
+			putchar(n++);
+			if (n != ':')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
