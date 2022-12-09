26a7976652a639e23798ae48b829a92deb7823d1
trainingmb <availfolly@yahoo.com>
2022-12-09 14:06:26 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,40 +9,28 @@
  */
 int main(void)
 {
-		int n = '0';
-
-		for (; n <= '9'; n++)
+	int i = 0;
+	for (; i <= 99; i++)
+	{
+		int j = i + 1;
+		for (; j <= 99; j++)
 		{
-			int m;
-
-			for (m = n + 1; m <= '9'; m++)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
 			{
-				int p;
-
-				for (p = m + 1; p <= '9'; p++)
-				{
-					int q;
-
-					for (q = p + 1; q <= '9'; q++)
-					{
-						putchar(n);
-						putchar(m);
-						putchar(' ');
-						putchar(p);
-						putchar(q);
-						if (n == '7' && m == '7' && p == '8' && q == '9')
-						{
-							/* do nothing */
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
+				/* do nothing */
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 }
