8eb0510046e250d4d7747980484f99d2fb22b1e6
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-13 22:20:10 +0300

    Update 5-sign.c
    Betty coding style:
    5-sign.c:7: WARNING: please, no space before tabs
    5-sign.c:8: WARNING: please, no space before tabs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - Prints the sign of a number
  * @n: The number to be tested
  * Return:	Returns 1 and prints + if n is greater than zero
- * 		Returns 0 and prints 0 if n is zero
- * 		Returns -1 and prints - if n is less than zero
+ *			Returns 0 and prints 0 if n is zero
+ *			Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
