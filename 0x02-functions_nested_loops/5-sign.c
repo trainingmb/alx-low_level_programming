acf47cabea376ddce8325259a248cb4452e61c9a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-13 22:19:02 +0300

    Update 5-sign.c
    Betty coding corrections:
    5-sign.c:7: WARNING: please, no space before tabs
    5-sign.c:8: WARNING: please, no space before tabs
    5-sign.c:15: ERROR: parentheses are required on a return statement
    5-sign.c:20: ERROR: parentheses are required on a return statement
    5-sign.c:25: ERROR: parentheses are required on a return statement
    Betty Documentation:
    5-sign.c:10: warning: No description found for parameter or member 'n'
    5-sign.c:11: warning: Excess function parameter 'c' description in 'print_sign'

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be tested
+ * @n: The number to be tested
  * Return:	Returns 1 and prints + if n is greater than zero
- * 			Returns 0 and prints 0 if n is zero
- * 			Returns -1 and prints - if n is less than zero
+ * 		Returns 0 and prints 0 if n is zero
+ * 		Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 }
