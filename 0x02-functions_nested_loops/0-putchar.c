2a541324167d67386a47faa6fb7283da6363d117
trainingmb <availfolly@yahoo.com>
2022-12-13 10:03:04 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,7 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	char *n = "_putchar";
-
+	char n[] = "_putchar";
 	int i = 0;
 
 	for (i = 0 ; i < 8 ; i++)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
