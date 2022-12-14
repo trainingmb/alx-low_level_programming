28e3505c56562a4f57bc8f39bb05f8618a15a3d4
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-14 23:45:35 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,58 +2,81 @@
 
 
 /**
- * _putnumber - prints number provided
- * @i:		The number to print
- * @digits:	Number of digits to print
- * @filler:	The filler character before the number
- * Return:	No return
+ * printnumber - print vector a
+ * @a: int vector of length 30
+ * Return: No return
  */
-void _putnumber(unsigned long i, int digits, char filler)
+void printnumber(int a[])
 {
-	unsigned long ld = 10000000000000000000UL;
-	char flag = 0;
-	int dg = 20;
+	int i = 0, flag = 0;
 
-	while (ld >= 1)
+	for( i = 29; i >= 0; i--)
 	{
-		unsigned long mod = i % ld;
-		unsigned long div = i / ld;
-
-		if (flag || div > 0 || dg == 1)
+		if (flag || a[i] > 0)
 		{
-			_putchar(div + '0');
+			_putchar(a[i] + '0');
 			flag = 1;
 		}
-		else if (dg <= digits)
-		{
-			_putchar(filler);
-		}
-		i = mod;
-		ld = ld / 10;
-		dg--;
 	}
 }
-
 /**
- * main -  prints the first 50 Fibonacci numbers
+ * copy - copy vector b to vector a
+ * @a: int vector of length 30
+ * @b: int vector of length 30
+ * Return: No return
+ */
+void copy(int a[], int b[])
+{
+	int i = 0;
+
+	for( i = 0; i < 30; i++)
+	{
+		a[i] = b[i];
+	}
+}
+/**
+ * add - add vector b to vector a
+ * @a: int vector of length 30
+ * @b: int vector of length 30
+ * Return: No return
+ */
+void add(int a[], int b[])
+{
+	int i = 0, carry = 0, temp = 0;
+
+	for( i = 0; i < 30; i++)
+	{
+		temp = (a[i] + b[i] + carry) % 10;
+		carry = (a[i] + b[i] + carry) / 10;
+		a[i] = temp;
+	}
+}
+/**
+ * main -  prints the first 98 Fibonacci numbers
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	unsigned long n = 1, m = 2, c = 0, temp = 0;
+	int n[30] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int m[30] = {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int temp[30] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int c = 0;
 
-	_putnumber(n, 1, ' ');
+	printnumber(n);
 	while (c < 97)
 	{
 		_putchar(',');
 		_putchar(' ');
-		_putnumber(m, 1, ' ');
+		printnumber(m);
 		c++;
-		temp = m;
-		m += n;
-		n = temp;
+		copy(temp,m);
+		add(m,n);
+		copy(n,temp);
 	}
 	_putchar('\n');
-	return (0);
+	return (0);			
 }
