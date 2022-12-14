cde2b64be5f6eae2701efa7a2e2abdfa5098ec50
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-14 18:53:07 +0300

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+
+/**
+ * _putnumber - prints number provided
+ * @i:		The number to print
+ * @digits:	Number of digits to print
+ * @filler:	The filler character before the number
+ * Return:	No return
+ */
+void _putnumber(long long i, int digits, char filler)
+{
+	long long ld = 1000000000000000000;
+	char flag = 0;
+	int dg = 19;
+
+	while (ld >= 1)
+	{
+		long long mod = i % ld;
+		long long div = i / ld;
+
+		if (flag || div > 0 || dg == 1)
+		{
+			_putchar(div + '0');
+			flag = 1;
+		}
+		else if (dg <= digits)
+		{
+			_putchar(filler);
+		}
+		i = mod;
+		ld = ld / 10;
+		dg--;
+	}
+}
+
+/**
+ * main -  finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long n = 1, m = 2, c = 0, temp = 0;
+	long long sum = 0;
+
+	while (m <= 4000000)
+	{
+		if ((m % 2) == 0)
+		{
+			sum += m;
+		}
+		c++;
+		temp = m;
+		m += n;
+		n = temp;
+	}
+	_putnumber(sum, 1,' ');
+	_putchar('\n');
+	return (0);
+}
