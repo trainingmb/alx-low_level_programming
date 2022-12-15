49a3a6ca7508b84f47776f577329466522478b40
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-15 18:18:01 +0300

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if the passed inttger is a upper case letter
+ * @c: The letter to test
+ * Return: Returns 1 if is lower case else return 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
