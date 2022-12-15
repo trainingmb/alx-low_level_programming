1b74db35804ee5cc5b7d8dba1acdadba28b89365
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-15 20:19:59 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,60 +1,4 @@
-#include "main.h"
-
-
-/**
- * printFizz -  Prints Fizz
- */
-void printFizz(void)
-{
-	_putchar('F');
-	_putchar('i');
-	_putchar('z');
-	_putchar('z');
-}
-/**
- * printBuzz -  Prints Buzz
- */
-void printBuzz(void)
-{
-	_putchar('B');
-	_putchar('u');
-	_putchar('z');
-	_putchar('z');
-}
-
-/**
- * _putnumber - prints number provided
- * @i:		The number to print
- * @digits:	Number of digits to print
- * @filler:	The filler character before the number
- * Return:	No return
- */
-void _putnumber(int i, int digits, char filler)
-{
-	int ld = 1000000000;
-	char flag = 0;
-	int dg = 10;
-
-	while (ld >= 1)
-	{
-		int mod = i % ld;
-		int div = i / ld;
-
-		if (flag || div > 0 || dg == 1)
-		{
-			_putchar(div + '0');
-			flag = 1;
-		}
-		else if (dg <= digits)
-		{
-			_putchar(filler);
-		}
-		i = mod;
-		ld = ld / 10;
-		dg--;
-	}
-}
-
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -65,30 +9,30 @@ int main(void)
 {
 	int n = 2;
 
-	_putchar('1');
+	printf("1");
 	while (n < 101)
 	{
-		_putchar(' ');
+		printf(" ");
 		switch (((n % 3) == 0) + (2 * ((n % 5) == 0)))
 		{
 			case 0:
-				_putnumber(n, 1, ' ');
+				printf("%d", n);
 				break;
 			case 1:
-				printFizz();
+				printf("Fizz");
 				break;
 			case 2:
-				printBuzz();
+				printf("Buzz");
 				break;
 			case 3:
-				printFizz();
-				printBuzz();
+				printf("Fizz");
+				printf("Buzz");
 				break;
 			default:
 				break;
 		}
 		n++;
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
 }
