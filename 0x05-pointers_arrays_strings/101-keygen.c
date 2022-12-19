fa2e65eeb57c62faac86841943b17fa48c8bbe20
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-19 20:11:48 +0300

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints password for crakme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("HJjkYTtygyvjoHHgjtYJJNhUinhukbtlj");
+
+	return (0);
+}
