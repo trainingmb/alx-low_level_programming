891062edda7846e8775313b94a4f7d61ec2943eb
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-19 20:27:57 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,14 +1,54 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints password for crakme
+ * password - generate random uppercase, lowercase or number
+ * @c: length of password
+ * @pwd: generated password pointer
  *
- * Return: Always 0.
+ */
+void password(int c, char pwd[])
+{
+	int s;
+	int i = 0;
+
+	srand(time(NULL));
+
+	while (i < c)
+	{
+		s = rand() % 4;
+		switch (s)
+		{
+			case 0:
+				pwd[i] = ((rand() % 26) + 'a');
+				break;
+			case 1:
+				pwd[i] = ((rand() % 26) + 'A');
+				break;
+			case 2:
+				pwd[i] = (rand() % 9) + '0';
+				break;
+			case 3:
+				pwd[i] = ((rand() % 15) + 12);
+				break;
+		}
+		i++;
+	}
+}
+
+/**
+ * main - generate valid password
+ *
+ * Return: Always return 0
  */
 int main(void)
 {
-	printf("HJjkYTtygyvjoHHgjtYJJNhUinhukbtlj");
+	char pwd[24];
+
+	password(23, pwd);
+	pwd[23] = '\0';
+	printf("%s\n", pwd);
 
 	return (0);
-}
+} 
