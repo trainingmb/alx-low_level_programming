2932612bea4f99fbfc6d83ca34a52ec2b955b88f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-19 17:07:23 +0300

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Returns the length of a string.
+ * @str : String pointer
+ */
+void _puts(char *str)
+{
+	int siz = 0;
+
+	while (str[siz] != '\0')
+	{
+		_putchar(str[siz++]);
+	}
+	_putchar('\0');
+}
