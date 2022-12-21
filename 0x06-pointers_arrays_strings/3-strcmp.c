1abb7cd5cab95ee813037fff77fc12c1cb0a89d3
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-21 14:43:22 +0300

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - work exactly like strcmp,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest : String pointer destination
+ * @src : String pointer source
+ * Return: return integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int siz = 0, i = 0, j = 0, ret = 0;
+
+	for(i = 0; s1[i] != '\0'; i++)
+	{
+		if (s2[j] != '\0')
+		{
+			ret += s1[i] - s2[j];
+			j++;
+		}
+	}
+	return (ret);
+}
