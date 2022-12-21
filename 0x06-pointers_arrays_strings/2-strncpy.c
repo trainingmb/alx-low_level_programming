09afdfa0f3a819075742870aa5ccec9fcc385051
trainingmb <118762148+trainingmb@users.noreply.github.com>
2022-12-21 13:58:33 +0300

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy two strings,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest : String pointer destination
+ * @src : String pointer source
+ * @n : Integer maximum no of bytes
+ * Return: return destination pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int siz = 0, i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[siz] = src[i];
+		i++;
+		siz++;
+	}
+	dest[siz] = '\0';
+	return (dest);
+}
