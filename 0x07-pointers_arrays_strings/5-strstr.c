3050cb38e14dd47a9770c29b8a03d33c91a78bc0
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-03 13:35:15 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * in - that gets the length of a prefix substring
+ * @c: the pointer for the string
+ * @a: the pointer to substring
+ *
+ * Return: If c in a
+ */
+char in(char *c, char *a)
+{
+	unsigned int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (c[i] != a[i])
+		{
+			return (0);
+		}
+		i++;
+	}
+
+	return (1);
+}
+
+
+/**
+ * _strpbrk - locates the first occurrence in
+ * the string s of any of the bytes in the string
+ * @haystack: the pointer for the string
+ * @needle: the pointer containing the bytes
+ *
+ * Return: a pointer to the byte in s that
+ * matches one of the bytes in accept, or
+ * NULL if no such byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		if (in(&haystack[i], needle))
+		{
+			return (&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
