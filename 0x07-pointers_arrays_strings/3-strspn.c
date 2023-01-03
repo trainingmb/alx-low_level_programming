afca3936107e5fe3d29e400a2be27c3f0883c698
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-03 13:16:53 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,45 @@
+/**
+ * in - that gets the length of a prefix substring
+ * @c: the pointer for the string
+ * @accept: the pointer containing the bytes
+ *
+ * Return: If c in accept
+ */
+char in(char c, char *accept)
+{
+	unsigned int i;
+
+	i = 0;
+	while (accept[i] != '\0')
+	{
+		if (c == accept[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+
+	return (0);
+}
+
+
+/**
+ * _strspn - that gets the length of a prefix substring
+ * @s: the pointer for the string
+ * @accept: the pointer containing the bytes
+ *
+ * Return: the number of bytes in the initial
+ * segment of s which consist only of bytes from
+ * accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	i = 0;
+	while (in(s[i], accept))
+	{
+		i++;
+	}
+	return (i);
+}
