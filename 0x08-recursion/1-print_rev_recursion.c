1894aa7512217fee7eec1cdec9c237e09e63dec1
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-05 12:03:05 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,37 +1,18 @@
 #include <unistd.h>
 /**
- * _pr - prints a string in reverse,
- * followed by a new line
- * @s: string
- */
-void _pr(char *s)
-{
-	if (s[0] == '\0')
-	{
-		char c = '\n';
-
-		write(1, &c, 1);
-	}
-	else
-	{
-		_pr(&s[1]);
-		write(1, s, 1);
-	}
-}
-
-/**
  * _print_rev_recursion - prints a string in reverse,
  * followed by a new line
  * @s: string
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\n')
+	if (s[0] == '\0')
 	{
-		_pr(&s[1]);
+		;
 	}
 	else
 	{
-		_pr(s);
+		_print_rev_recursion(&s[1]);
+		write(1, s, 1);
 	}
 }
