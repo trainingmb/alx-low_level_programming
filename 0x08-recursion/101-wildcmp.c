970a2c0ef909c9218d377f141dee0488ec6905c9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-06 16:36:34 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,17 +8,15 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (s1[0] == '\0' && (s2[0] == '*' || s2[0] == '\0'))
+	if (s1[0] == '\0')
 	{
-		int i = 0;
-		
-		while (s2[i] == '*')
-		{
-			i++;
-		}
-		if (s2[i] == '\0')
+		if (s2[0] == '\0')
 		{
 			return (1);
+		}
+		else if(s2[0] == '*')
+		{
+			return (wildcmp(&s1[0], &s2[1]));
 		}
 		else
 		{
