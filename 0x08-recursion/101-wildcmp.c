09e0e1c50e6bd4a5fa8b5cc1fb2f216e4c91cf46
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-06 16:25:10 +0300

    Advanced

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,45 @@
+
+/**
+ * wildcmp - dfdgdg
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: return similar
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1[0] == '\0' && (s2[0] == '*' || s2[0] == '\0'))
+	{
+		int i = 0;
+		
+		while (s[i] == '*')
+		{
+			i++;
+		}
+		if (s[i] == '\0')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (s1[0] == s2[0])
+	{
+		return (wildcmp(&s1[1], &s2[1]));
+	}
+	else if (s2[0] == '*')
+	{
+		int a, b, c;
+		
+		a = (wildcmp(&s1[0], &s2[1]));
+		b = (wildcmp(&s1[1], &s2[1]));
+		a = (wildcmp(&s1[1], &s2[0]));
+		return a || b || c;
+	}
+	else
+	{
+		return (0);
+	}
+}
