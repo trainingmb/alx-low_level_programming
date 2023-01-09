c6105c13789cfcdc9149256d08fb2f54ba77e26d
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-09 10:08:17 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,24 +12,24 @@ int main(int argc, char *argv[])
 	int n = 0, coins = 0, i, j;
 	int denoms[] = {25, 10, 5, 2, 1};
 
-	if(argc != 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
 	n = atoi(argv[1]);
-	if((n == 0) && !(argv[1][0] == '0'))
+	if ((n == 0) && !(argv[1][0] == '0'))
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if(n < 0)
+	if (n < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for(i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		j = n / denoms[i];
 		coins += j;
