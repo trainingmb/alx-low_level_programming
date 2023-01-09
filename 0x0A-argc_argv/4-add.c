95d38ca1d6bba5a50e2daf175cbd9ebc9feb4cb1
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-09 10:06:38 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,14 +9,14 @@
  **/
 int main(int argc, char *argv[])
 {
-	int n = 0,m = 0,i = 1;
+	int n = 0, m = 0, i = 1;
 
-	if(argc == 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	if(argc < 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	while (i < argc)
 	{
 		m = atoi(argv[i]);
-		if((m == 0) && !(argv[i][0] == '0'))
+		if ((m == 0) && !(argv[i][0] == '0'))
 		{
 			printf("Error\n");
 			return (1);
