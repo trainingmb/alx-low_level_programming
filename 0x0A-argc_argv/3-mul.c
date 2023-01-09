d61f73ef1b77f7a37d4c8f53b933865da135b41f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-09 09:23:25 +0300

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Argument Count
+ * @argv: A pointer array of size argument count
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int n,m;
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	printf("%d\n", n*m);
+
+	return (0);
+}
