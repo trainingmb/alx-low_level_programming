443ea42f5874fb0d47ed56a7fc4040dad6a846db
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-12 15:55:46 +0300

    Create 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+/**
+ * **ss - ss
+ * @str: argument count
+ * Return: string
+ **/
+char *sss(char *str)
+{
+    int i, size;
+    char* ret;
+
+    size = 0;
+    for (i = 0; str[i] != ' '; i++)
+    {
+        size++;
+    }
+    ret = malloc((char) * (size + 1));
+    if (ret == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; str[i] != ' '; i++)
+    {
+        ret[i] = str[i];
+    }
+    ret[size] = '\0';
+    return (ret);
+}
+/**
+ * countstr - count strings
+ * @s: string
+ * Return: count of stringd
+ **/
+int countstr(char *s)
+{
+    int no, i;
+
+    no = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ')
+        {
+            no++;
+            while (s[i] == ' ')
+            {
+                i++;
+            }
+        }
+    }
+    if (s[i-1] != ' ')
+    {
+        no++;
+    }
+    return (no);
+}
+
+/**
+ * **strtow - concatenates all the arguments of your program
+ * @str: argument count
+ * Return: array of strings
+ **/
+char **strtow(char *str)
+{
+    int no, i, k;
+    char ** ret;
+
+    no = 0, k = 0;
+    while (str[i] == ' ')
+    {
+        i++;
+    }
+    no = countstr(&str[i]);
+    ret = malloc((char*) * (no));
+    if (ret == NULL)
+    {
+        return (NULL);
+    }
+    for (i = i; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            k++;
+            while (str[i] == ' ')
+            {
+                i++;
+            }
+        }
+        else
+        {
+            ret[k] = sss(&str[i]);
+            if (ret[k] == NULL)
+            {
+                return (NULL);
+            }
+            while (str[i] != ' ')
+            {
+                i++;
+            }
+        }
+    }
+}
