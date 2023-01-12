05199d50215b062bdc27916fa342ffc269e65e2c
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-12 15:10:56 +0300

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: pointer to strings
+ * Return: one sring or null
+ **/
+char *argstostr(int ac, char **av)
+{
+	char *largeStr;
+	int i, j, k, size;
+
+	size = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+		size++;
+	}
+	size++;
+
+	largeStr = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			largeStr[k++] = av[i][j];
+		}
+		largeStr[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
+}
