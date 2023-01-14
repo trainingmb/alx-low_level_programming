f041a9d39ef2b17a840621b089c841851c18aa52
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-14 04:12:57 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -52,7 +52,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	size = (new_size > old_size? old_size: new_size);
+	size = (new_size > old_size ? old_size : new_size);
 	ret = malloc(new_size);
 	if (ret == NULL)
 	{
