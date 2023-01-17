3415fb8c23959add53555c94f15a6d8e74c5fc0a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-17 23:24:27 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ret = (dog_t *)malloc(sizeof(dog_t));
 	if (ret == NULL)
-		return NULL;
+		return (NULL);
 	len = i = 0;
 	while (name[len++])
 		;
