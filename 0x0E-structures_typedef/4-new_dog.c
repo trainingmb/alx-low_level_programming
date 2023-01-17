c99a84e3a42a026b0ee6851091bec7e4a013e680
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-17 23:22:35 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,42 +10,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ret;
-	unsigned int len;
+	unsigned int len, i;
 
 	ret = (dog_t *)malloc(sizeof(dog_t));
 	if (ret == NULL)
 		return NULL;
-	len = 0;
+	len = i = 0;
 	while (name[len++])
 		;
-	ret->name = malloc(sizeof(char) * len);
+	ret->name = malloc(sizeof(char) * (len + 1));
 	if (ret->name == NULL)
 	{
 		free(ret);
 		return (NULL);
 	}
-	while (len != 0)
-	{
-		ret->name[len] = name[len];
-		len--;
-	}
+	for (i = 0; i < len; i++)
+		ret->name[i] = name[i];
 	ret->name[len] = name[len];
 	ret->age = age;
 	len = 0;
 	while (owner[len++])
 		;
-	ret->owner = malloc(sizeof(char) * len);
+	ret->owner = malloc(sizeof(char) * (len + 1));
 	if (ret->owner == NULL)
 	{
 		free(ret->name);
 		free(ret);
 		return (NULL);
 	}
-	while (len != 0)
-	{
-		ret->owner[len] = owner[len];
-		len--;
-	}
+	for (i = 0; i < len; i++)
+		ret->owner[i] = owner[i];
 	ret->owner[len] = owner[len];
 	return (ret);
 }
