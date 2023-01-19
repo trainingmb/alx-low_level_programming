9e56f21be6c2ae00b95309c033825a1321f7cf99
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-19 17:40:43 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTER_HEADER
 #define FUNCTION_POINTER_HEADER boing
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
