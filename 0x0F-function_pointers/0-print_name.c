14309aaad590cfed573f8bad23a5bda83127126c
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-19 17:42:07 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 /**
  * print_name - a function that prints a name
  * @name: char pointer to the name to print
- * @f: function pointer to name printing function 
+ * @f: function pointer to name printing function
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	if( f != ((void *) 0) && name != ((void *) 0) && name[0] != '\0')
+	if(f != ((void *) 0) && name != ((void *) 0) && name[0] != '\0')
 		f(name);
 }
