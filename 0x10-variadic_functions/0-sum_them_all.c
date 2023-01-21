c52ef500d60227e033d703d26177b99708ebbbd5
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-21 17:49:25 +0300

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_function.h"
+
+/**
+ * sum_them_all - a function that searches for an integer
+ * @n: number of integers
+ * Return: the sum of the passed integers
+ * or not return 0
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list numbers;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	sum = 0;
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIADIC_FUNCTIONS_HEADER
+#define VARIADIC_FUNCTIONS_HEADER boing
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+#endif
