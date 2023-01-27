b650855c8c3038705b534bb8a478fb8a798940b5
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-27 21:20:55 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -4,14 +4,30 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-struct modifer
+/**
+ * struct modifer - Modifier
+ *
+ * @left_just: is left justified
+ * @filler: the filler character
+ * @sign: should be signed
+ * @width: minimum width
+ * @endindx: end index in the string
+ */
+typedef struct modifer
 {
 	char left_just;
 	char filler;
 	char sign;
 	unsigned int width;
 	unsigned int endindx;
-};
+} modif;
+
+/**
+ * struct sel - Selection option
+ *
+ * @seq: identifying seq
+ * @f: fucntion to call
+ */
 typedef struct sel
 {
 	char seq;
