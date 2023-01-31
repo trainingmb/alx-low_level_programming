6c533cc48b09ffbe2ea55ad36fb2a786d1f6216e
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-01-31 11:26:00 +0300

    Update 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - get length f=of singly linked list
+ * @h: the head of the list
+ * Return: the length of the list
+ */
+size_t list_len(const list_t *h)
+{
+    size_t s;
+    const list_t *iter;
+
+    iter = h;
+    for (s = 0; iter != ((void *) 0); iter = iter->next);
+    return (s);
+}
