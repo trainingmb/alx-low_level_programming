113949f0b64ec934af68e5a3abfbfdf5f6dee968
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-01 19:59:52 +0300

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print a singly list of int
+ * @h: the start node
+ * Return: The length of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+	const listint_t *iter;
+
+	iter = h;
+	if (h == ((void *) 0))
+		return (0);
+	else
+	{
+		for (n = 0; iter != ((void *) 0); n++)
+		{
+			printf("%i\n", iter->n);
+			iter = iter->next;
+		}
+	}
+	return (n);
+}
