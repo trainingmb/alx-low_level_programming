8b39d5090fbeaa238ca43c5e6108ca12dfe04719
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-06 13:56:30 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x;
 	const char  *iter;
-	
+
 	x = 0;
 	if (b == ((void *)0))
-	{
 		return (0);
-	}
 	for (iter = b; iter[0] != '\0'; iter++)
 	{
 		if (iter[0] == '0')
-			x = x*2;
+			x = x * 2;
 		else if (iter[0] == '1')
-			x = x*2 + 1;
+			x = x * 2 + 1;
 		else
 			return (0);
 	}
