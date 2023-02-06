216aa6f1908d58ab9d8a69391584142f62126024
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-06 13:50:41 +0300

    0. 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+
+/**
+ * binary_to_uint: turn a binary string to an integer
+ * @b: the binary string
+ * Return: Either the int equivalent or NULL if not valid
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x;
+	const char  *iter;
+	
+	x = 0;
+	if (b == ((void *)0))
+	{
+		return (0);
+	}
+	for (iter = b; iter != '\0'; iter++)
+	{
+		if (iter[0] == '0')
+			x = x*2;
+		else if (iter[0] == '1')
+			x = x*2 + 1;
+		else if (iter[0] == '\0')
+			break;
+		else
+			return (0);
+	}
+	return (x);
+}
