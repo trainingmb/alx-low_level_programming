7b2b7ccaa0dd2a7a160f52890615fd94bb778dc9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-06 17:14:12 +0300

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+
+/**
+ * get_bit: Get the bit at index
+ * @n: pointer to the number
+ * @index: index of bit
+ * Return: 1 or 0 if successful else -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index < 31)
+	{
+		i = 1 << index;
+		return ((n | i) != 0);
+	}
+	return (-1);
+}
