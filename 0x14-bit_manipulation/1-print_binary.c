36ddbc7b40aacf0ff2ce05ac662afaf0cc71f0c2
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-06 15:57:34 +0300

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+
+#include "main.h"
+
+/**
+ * print_binary: print the binary number equivalent of n
+ * @n: the number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	char pr;
+
+	pr = 0;
+	i = 1 << 30;
+	while (i)
+	{
+		if(pr == 0)
+		{
+			if ((n & i))
+			{
+				pr = 1;
+				_putchar('1');
+			}
+			i = i >> 1;
+		}
+		else
+		{
+			if ((n & i))
+				_putchar('1');
+			else
+				_putchar('0');
+			i = i >> 1;
+		}
+	}
+	if(pr == 0)
+	{
+		_putchar('0');
+	}
+}
