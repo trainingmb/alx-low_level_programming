35e2c672d672d717e47bdd5cb0d2b512658bfd20
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-08 23:26:45 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * read_textfile - read from a text file and write to std out
+ * @filename: The name of the file to read from
+ * @letters: the number of letters to read
+ * Return: The size of the file in char count
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
