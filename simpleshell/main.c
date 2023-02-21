6af60375225f437bb5b552d2c0d8dd1f56eef60b
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-21 22:35:21 +0300

    simpleshell

---
+++ simpleshell/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main
+ * @argc: Argument count
+ * @argv: Argument list
+ * Return: Return successful
+ */
+int main(int argc, char **argv)
+{
+    char status;
+
+    puts("#cisfun$ ");
+
+    return EXIT_SUCCESS;
+}
