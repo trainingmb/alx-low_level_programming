13dc696ed763be81dfaae88075678e830f39127a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-22 22:27:57 +0300

    Update execute.c

--- simpleshell/execute.c
+++ simpleshell/execute.c
@@ -14,7 +14,7 @@ int execute(char **argv, char **env)
 	childpid = fork();
 	if (childpid == 0)
 	{
-		if (execve("/bin/ls", argv, env) == -1)
+		if (execve(argv[0], argv, env) == -1)
 		{
 			len = _strlen(argv[0]);
 			argv[0][len - 1] = '\0';
