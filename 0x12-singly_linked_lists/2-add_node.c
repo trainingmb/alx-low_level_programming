35acbbb55e0ac0904c922c666452922db16aa529
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-24 18:18:52 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -53,7 +53,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *iter;
 
-	if (str == NULL)
+	if (str == NULL || head == NULL)
 		return (NULL);
 	iter = create_node(str, (*head));
 	return (iter);
