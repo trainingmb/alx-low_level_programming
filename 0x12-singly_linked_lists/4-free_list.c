387d9cd793fc0975d80a4f4443d54a367cd7e5c8
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-24 18:18:54 +0300

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - Free a linked list
+ * @head: The first node
+ */
+void free_list(list_t *head)
+{
+	list_t *iter;
+
+	while(head)
+	{
+		iter = head;
+		free(iter->str);
+		head = iter->next;
+		free(iter);
+	}
+}
