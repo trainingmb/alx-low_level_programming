fc65263d60fce3f56ae997a98b8a75275f4bbba7
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-24 17:53:13 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,6 +24,6 @@ int _putuint(unsigned int u);
 size_t print_list(const list_t *h);
 unsigned int _pow(unsigned int u, int n);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
 
 #endif
