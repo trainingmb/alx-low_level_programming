ec1d07288dc8a90f4fcbf6cd1fac189f9dc483a7
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-24 17:50:06 +0300

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+
+/**
+ * _strlen - Gets length of string
+ * @s: TO get length
+ * Return: length of the string
+ */
+size_t _strlen(const char *s)
+{
+	size_t len;
+
+	for (len = 0; s[len]; len++);
+	return (len);
+}
+
+/**
+ * create_node - Create a node
+ * @str: String to add to list
+ * @next: The next node in the list
+ * Return: new list node
+ */
+list_t *create_node(const char *str, list_t * next)
+{
+	list_t *newnode;
+	size_t s, len;
+
+	if (str == NULL)
+		return (NULL);
+	len = _strlen(str);
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = malloc((len + 1) * sizeof(char));
+	newnode->len = len;
+	if (newnode->str == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	for (s = 0; s <= len; s++)
+		newnode->str[s] = str[s];
+	newnode->next = next;
+	return (newnode);
+}
+
+/**
+ * add_node - Add a node to the list at the 
+ * @head: the head of the list
+ * @str: String to add to list
+ * Return: new list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t s, len;
+	list_t *iter;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	iter = create_node(str, (*head));
+	return (iter);
+}
