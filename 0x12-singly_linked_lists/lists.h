301625a5db478ff33be6d2dfddbe4b419139f60f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-02-25 15:02:48 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define MAIN_H_HEADER boing
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,9 +18,14 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
+void _putstr(char *str);
+void _putstrlen(char *str, unsigned int len);
+int _putuint(unsigned int u);
 size_t print_list(const list_t *h);
 unsigned int _pow(unsigned int u, int n);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
