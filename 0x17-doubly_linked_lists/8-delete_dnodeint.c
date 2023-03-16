e8950929c7f87ec28c5ac1937db2e4b642500eff
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-17 00:07:39 +0300

    Create 8-delete_dnodeint.c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at
+ * index index of a dlistint_t linked list.
+ * @head: pointer to the start node
+ * @index: The node to pop
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	dlistint_t *iter;
+	unsigned int count;
+
+
+	if (*head == ((void *) 0))
+		return (-1);
+
+	count = 0;
+	for (iter = (*head)
+		; iter->next != ((void *) 0) && ((count + 1) < index)
+		; iter = iter->next)
+	{
+		count++;
+	}
+	if (index == 0)
+	{
+		*head = iter->next;
+		*head->prev = ((void *) 0);
+		free(iter);
+		return (1);
+	}
+	if (((count + 1) != index) || (iter->next == ((void *) 0)))
+	{
+		return (-1);
+	}
+	temp = iter->next;
+	iter->next = temp->next;
+	free(temp);
+	return (1);
+}
