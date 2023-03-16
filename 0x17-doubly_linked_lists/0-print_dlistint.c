78402059c1d94f2cf4070c7c8b20f984f6e3fece
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-16 20:08:33 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_dlistint - Print a doubleylinkedlist
- * h@: the start node
+ * @h: the start node
  * Return: The length of the list
  */
 size_t print_dlistint(const dlistint_t *h)
