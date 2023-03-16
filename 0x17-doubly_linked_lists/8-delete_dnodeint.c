ced6389096531e0abfb5692d217baf78f3f6c2fd
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-17 00:12:21 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = iter->next;
-		*head->prev = ((void *) 0);
+		(*head)->prev = ((void *) 0);
 		free(iter);
 		return (1);
 	}
