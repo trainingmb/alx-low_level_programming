3971e0b6e8238015e33e3a1d6d058822075c67e9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-16 20:02:06 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Get list length
+ * @h: ths start node
+ * Return: The length of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n;
+	const listint_t *iter;
+
+	iter = h;
+	if (h == ((void *) 0))
+		return (0);
+	for (n = 0; iter != ((void *) 0); n++)
+	{
+		iter = iter->next;
+	}
+	return (n);
+}
