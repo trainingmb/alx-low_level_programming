9cd7d8a8e326dbfe031fb4fc558bdab410e6d725
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-08 00:42:39 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,32 +24,31 @@ unsigned long int str_len(const char *key)
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key,
-	const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int indx;
-	hash_node_t *nn;
+	unsigned long index, size;
+	hash_node_t *new_node;
 
-	if (ht == NULL)
+	if (ht  == NULL || key == NULL || value == NULL)
 		return (0);
-	if ((key == NULL) || (str_len(key) > 0))
-		return (0);
-	indx = key_index((const unsigned char *)key, ht->size);
-	if ((ht->array[indx] != NULL) &&
-		(strcmp(ht->array[indx]->key, key)) == 0)
+
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
-		ht->array[indx]->value = strdup(value);
+		ht->array[index]->value = strdup(value);
 		return (1);
 	}
-	else
-	{
-		nn = malloc(sizeof(hash_node_t));
-		if (nn == NULL)
-			return (0);
-		nn->key = strdup(key);
-		nn->value = strdup(value);
-		nn->next = ht->array[indx];
-		ht->array[0] = nn;
-		return (1);
-	}
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
