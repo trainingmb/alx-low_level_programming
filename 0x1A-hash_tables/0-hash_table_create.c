c1d21b10b8fab7a684512e8c303adbe194e1d269
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-07 23:14:17 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,11 +7,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newtable = malloc(sizeof(hash_table_t));
 	if (newtable == ((void *) 0))
 		return ((void *) 0);
-	hash_table_t->size = size;
-	hash_table_t->array = ((void *) 0);
+	newtable->size = size;
+	newtable->array = ((void *) 0);
 
-	hash_table_t->array = malloc(size * hash_node_t);
-	if (hash_table_t->array == ((void *) 0))
+	newtable->array = malloc(size * hash_node_t);
+	if (newtable->array == ((void *) 0))
 	{
 		free(newtable);
 		return ((void *) 0);
