3bb846e5bfcef18569430aa5c8d8ab3572038db7
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-07 23:03:12 +0300

    Create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t * newtable;
+
+	newtable = malloc(sizeof(hash_table_t));
+	if (newtable == ((void *) 0))
+		return ((void *) 0);
+	hash_table_t->size = size;
+	hash_table_t->array = ((void *) 0);
+
+	hash_table_t->array = malloc(size * hash_node_s);
+	if (hash_table_t->array == ((void *) 0))
+	{
+		free(newtable);
+		return ((void *) 0);
+	}
+	return (newtable);
+}
