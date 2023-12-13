9c6cd0c256b575e17a74c0d81a57286cad1fc669
trainingmb <18762148+trainingmb@users.noreply.github.com>
2023-12-13 13:40:10 -0800

    Added 100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Search an ordered list in jump
+ * @array: Pointer to the array to be searched
+ * @size: The size of the array to be searched
+ * @value: The value for which the array is searching
+ * Return: The index where the value was found or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t gap = sqrt(size), i = 0, j = 0;
+
+	while (array[i] < value && i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i += gap;
+	}
+	j = i;
+	i -= gap;
+	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
+	while (array[i] < value && i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i++;
+	}
+	if (array[i] == value && i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		return (i);
+	}
+
+	return (-1);
+}
+
